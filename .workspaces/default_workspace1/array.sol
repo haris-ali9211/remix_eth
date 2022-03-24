e66cc4355493a9a11916f75d214074098a379151
Haris Ali <64413337+haris-ali9211@users.noreply.github.com>
2022-03-24 12:24:22 +0500

    dynamic_arr

---
+++ .workspaces/default_workspace1/array.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+
+contract array
+{
+    uint[4] public arr = [10,22,32,11];
+
+    function setter(uint index, uint value) public
+    {
+        arr[index] = value ; 
+    }
+
+    function length() public view returns(uint)
+    {
+        return arr.length;
+    }
+}
